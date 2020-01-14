0b960845d7682158efab5838f30239421de20441
lydiaswan15 <lydiaswan15@gmail.com>
2020-01-14 12:46:00 -0700

    changes

--- Assignments/W01/scriptW01.js
+++ Assignments/W01/scriptW01.js
@@ -5,3 +5,8 @@ const year = d.getFullYear();
 const output = year + " | Lydia Swanson | ";
 
 document.getElementById("bottomSection").textContent = output;
+
+var string = document.lastModified;
+let olastM = new Date(document.lastModified);
+
+document.getElementById("lastUpdated").innerHTML = olastM;
