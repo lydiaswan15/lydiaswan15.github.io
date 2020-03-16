813d7119c7732a9fdd3154f0b1780a5374ee843b
lydiaswan15 <lydiaswan15@gmail.com>
2020-03-16 11:44:36 -0600

    adding the index page

--- lesson9/scripts/towns.js
+++ lesson9/scripts/towns.js
@@ -1,20 +1,45 @@
 fetch('https://byui-cit230.github.io/weather/data/towndata.json')
-.then(result => {
-    return result.json();
-})
-.then(resultJSON => {
-    let towns = resultJSON.towns;
+    .then(result => {
+            return result.json();
+    })
+    .then(townsList => {
+            let towns = townsList.towns;
+            console.log(towns);
+        
+            towns.forEach(town =>{
+                let article = document.createElement('article');
+                let h2 = document.createElement('h2');
+                h2.textContent = `${town.name}`;
+                article.appendChild(h2);
 
-    towns.forEach(
-        town => {
-            if(town.name === 'Fish Haven'){
-                document.querySelector('#fishHavenName').textContent = town.name;
-                document.querySelector('#fishHavenMotto').textContent = town.moto;
-                document.querySelector('#fishHavenYearFounded').textContent = `Year Founded: ${town.yearFounded}`;
-                document.querySelector('#fishHavenPopulation').textContent = `Population: ${town.population}`;
-                document.querySelector('#fishHavenName').textContent = town.name;
-                document.querySelector('#fishHavenAnnualRainfall').textContent = `Annual Rainfall: ${town.averageRainfall}`;
-            } else if(town.name === 'Preston')
-        }
-    );
-})
+                let motto = document.createElement('p');
+                motto.textContent = `${town.motto}`;
+                article.appendChild(motto);
+
+                let yearFounded = document.createElement('p');
+                yearFounded.textContent = `Year Founded: ${town.yearFounded}`;
+                article.appendChild(yearFounded);
+
+                let currentPopulation = document.createElement('p');
+                currentPopulation.textContent = `Population: ${town.currentPopulation}`;
+                article.appendChild(currentPopulation);
+
+                let averageRainfall = document.createElement('p');
+                averageRainfall.textContent = `Average Rainfall: ${town.averageRainfall}`;
+                article.appendChild(averageRainfall);
+
+                let image = document.createElement('img');
+                image.setAttribute('src', `images/${town.photo}`);
+                image.setAttribute('alt', `${town.name}`);
+                article.appendChild(image);
+
+                if(town.name === "Preston"){
+                    document.getElementById('towns').appendChild(article);
+                }else if(town.name === "Fish Haven"){
+                    document.getElementById('towns').appendChild(article);
+                }else if(town.name === "Soda Springs"){
+                    document.getElementById('towns').appendChild(article);
+                }
+
+            })
+    })
