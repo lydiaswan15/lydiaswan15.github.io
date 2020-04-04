43d249d682c3fd4ffff03905d2fb9028cb4f4f3f
lydiaswan15 <lydiaswan15@gmail.com>
2020-04-03 20:59:13 -0600

    a;aslkdjf

--- hotelChain/scripts/temples.js
+++ hotelChain/scripts/temples.js
@@ -4,6 +4,25 @@ fetch(templeList)
     .then(result => {
             return result.json();
     })
-    .then(templesList => {
-            console.log(templesList);
+    .then(temples => {
+            const templesList = temples.temples;
+            console.log(temples);
+            templesList.forEach(oneTemple => {
+                let closureDates = "";
+                if(oneTemple.name === "Barranquilla Columbia"){
+                        closureDates = document.getElementById("barraquillaColumbiaClosure"); 
+                }   
+        
+                else if(oneTemple.name === "Logan Utah"){
+                        closureDates = document.getElementById("loganClosure"); 
+                } 
+
+                else if(oneTemple.name === "Laie Hawaii"){
+                        closureDates = document.getElementById("hawaiiClosure");
+                }
+                    oneTemple.templeClosure.forEach(date =>{
+                            let newItem = `<li>${date}</li>`
+                            closureDates.innerHTML += newItem;
+                    });
+            });
     });
