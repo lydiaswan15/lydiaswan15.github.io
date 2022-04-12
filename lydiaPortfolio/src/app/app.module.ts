be5c3e7dde723002fe56b859d87b5cee16173bda
lydiaswan15 <lydiaswan15@gmail.com>
2022-04-12 13:30:32 -0600

    starting some basic stying- a lot still to do.

--- lydiaPortfolio/src/app/app.module.ts
+++ lydiaPortfolio/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { HeadingComponent } from './components/heading/heading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    AboutMeComponent,
+    HeadingComponent
   ],
   imports: [
     BrowserModule
---
+++ lydiaPortfolio/src/app/components/about-me/about-me.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HeadingComponent } from '../heading/heading.component';
+
+@Component({
+  selector: 'app-about-me',
+  templateUrl: './about-me.component.html',
+  styleUrls: ['./about-me.component.css']
+})
+export class AboutMeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
